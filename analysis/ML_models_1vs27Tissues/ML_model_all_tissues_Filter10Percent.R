89623824c6d9a6580dd656bc388b63e8b7686fe8
Anaskou <75255312+Anaskou@users.noreply.github.com>
2024-04-23 12:52:50 +0300

    Update ML_model_all_tissues_Filter10Percent.R

--- analysis/ML_models_1vs27Tissues/ML_model_all_tissues_Filter10Percent.R
+++ analysis/ML_models_1vs27Tissues/ML_model_all_tissues_Filter10Percent.R
@@ -109,7 +109,7 @@ core=unique(core)
 #Parallel computing
 library(parallel)
 library(doMC) # for parallel computing
-numCores <- detectCores()
+numCores = detectCores()
 #Set how many cores the script will use (10 cores)
 registerDoMC(cores=10)
 
@@ -126,11 +126,11 @@ for (tissue in unique(metadata2$tissue_type)) {
     ####################################################################################################################################
     ################################## Split the datasets to train and testing dataset 
     
-    index <- createDataPartition(metadata2$tissue_type, p = 0.7, list = FALSE)
-    trainX <- all_sub[,index]
-    metadata_train <- metadata2[index,]
-    testX <- all_sub[,-index]
-    metadata_test <- metadata2[-index,]
+    index = createDataPartition(metadata2$tissue_type, p = 0.7, list = FALSE)
+    trainX = all_sub[,index]
+    metadata_train = metadata2[index,]
+    testX = all_sub[,-index]
+    metadata_test = metadata2[-index,]
     dim(trainX)
     dim(testX)
     table(metadata_train$tissue_type)
@@ -149,7 +149,7 @@ for (tissue in unique(metadata2$tissue_type)) {
     residuals_train2=residuals_train[core,]
     
     
-    #Train dataset
+    #Test dataset
     metaSeqObject = newMRexperiment(testX) 
     metaSeqObject_CSS  = cumNorm( metaSeqObject , p=cumNormStatFast(metaSeqObject) )
     dge_normalised = data.frame(MRcounts(metaSeqObject_CSS, norm=TRUE, log=FALSE))
@@ -168,35 +168,35 @@ for (tissue in unique(metadata2$tissue_type)) {
   samplingStrategy = "up"
   
   rownames(metadata_train) = metadata_train$specimen_id
-  TypeComparison <- metadata_train$tissue_type
+  TypeComparison = metadata_train$tissue_type
   TypeString = tissue
-  TypeComparisonFactor <- factor(ifelse(TypeComparison == TypeString, yes = TypeString, no = "OtherType"),
+  TypeComparisonFactor = factor(ifelse(TypeComparison == TypeString, yes = TypeString, no = "OtherType"),
                                  levels = c(TypeString, "OtherType"))
   
   metadata_train$TypeComparison=TypeComparisonFactor
-  mlDataY <- metadata_train
-  mlDataX <- data_trans2[rownames(mlDataY),]
+  mlDataY = metadata_train
+  mlDataX = data_trans2[rownames(mlDataY),]
   dim(mlDataY)[1] == dim(mlDataX)[1] # Sanity check
   
-  trainX <- mlDataX
-  trainY <- mlDataY$TypeComparison
+  trainX = mlDataX
+  trainY = mlDataY$TypeComparison
   
-  testX <- as.data.frame(t(residuals_test2))
-  TypeComparison <- metadata_test$tissue_type
+  testX = as.data.frame(t(residuals_test2))
+  TypeComparison = metadata_test$tissue_type
   TypeString = tissue
-  TypeComparisonFactor <- factor(ifelse(TypeComparison == TypeString, yes = TypeString, no = "OtherType"),
+  TypeComparisonFactor = factor(ifelse(TypeComparison == TypeString, yes = TypeString, no = "OtherType"),
                                  levels = c(TypeString, "OtherType"))
   
   metadata_test$TypeComparison=TypeComparisonFactor
   testY=metadata_test$TypeComparison
  
-  refactoredTrainY <- factor(gsub('([[:punct:]])|\\s+','',trainY))
-  refactoredTestY <- factor(gsub('([[:punct:]])|\\s+','',testY))
+  refactoredTrainY = factor(gsub('([[:punct:]])|\\s+','',trainY))
+  refactoredTestY = factor(gsub('([[:punct:]])|\\s+','',testY))
   
-  refactoredTrainY <- relevel(refactoredTrainY, ref = gsub('([[:punct:]])|\\s+','',TypeString))
-  refactoredTestY <- relevel(refactoredTestY, ref = gsub('([[:punct:]])|\\s+','',TypeString))
+  refactoredTrainY = relevel(refactoredTrainY, ref = gsub('([[:punct:]])|\\s+','',TypeString))
+  refactoredTestY = relevel(refactoredTestY, ref = gsub('([[:punct:]])|\\s+','',TypeString))
   
-  ctrl <- trainControl(method = "repeatedcv",
+  ctrl = trainControl(method = "repeatedcv",
                        number = 2,
                        repeats = 1,
                        summaryFunction = twoClassSummary,
@@ -209,14 +209,14 @@ for (tissue in unique(metadata2$tissue_type)) {
   ctrl$sampling = samplingStrategy
   print("Now training model with up sampling...")
   
-  defaultGBMGrid <-  expand.grid(interaction.depth = seq(1,3),
+  defaultGBMGrid =  expand.grid(interaction.depth = seq(1,3),
                                  n.trees = floor((1:3) * 50),
                                  shrinkage = 0.1,
                                  n.minobsinnode = 3)
   #Explained https://www.listendata.com/2015/07/gbm-boosted-models-tuning-parameters.html
   #Check this out https://s3.amazonaws.com/assets.datacamp.com/production/course_6650/slides/chapter2.pdf
   
-  mlModel <- train(x = trainX,
+  mlModel = train(x = trainX,
                    y = refactoredTrainY,
                    method = "gbm",
                    preProcess = c("scale","center"),
@@ -225,9 +225,9 @@ for (tissue in unique(metadata2$tissue_type)) {
                    tuneGrid = defaultGBMGrid)
   
   
-  predProbs <- as.numeric(predict(mlModel, newdata = testX, type = "prob")[, gsub('([[:punct:]])|\\s+','',TypeString)])
-  fg <- predProbs[refactoredTestY == gsub('([[:punct:]])|\\s+','',TypeString)]
-  bg <- predProbs[refactoredTestY == "OtherType"]
+  predProbs = as.numeric(predict(mlModel, newdata = testX, type = "prob")[, gsub('([[:punct:]])|\\s+','',TypeString)])
+  fg = predProbs[refactoredTestY == gsub('([[:punct:]])|\\s+','',TypeString)]
+  bg = predProbs[refactoredTestY == "OtherType"]
   
   roc_GTEX=roc.curve(scores.class0 = fg, scores.class1 = bg, curve = T)
   pr_GTEX=pr.curve(scores.class0 = fg, scores.class1 = bg, curve = T, rand.compute=T)
@@ -236,14 +236,14 @@ for (tissue in unique(metadata2$tissue_type)) {
   }
   
     # Calculate the standard error of the mean
-  se_mean_X1 <- sd(tissue_cumulative2$X1) / sqrt(length(tissue_cumulative2$X1))
+  se_mean_X1 = sd(tissue_cumulative2$X1) / sqrt(length(tissue_cumulative2$X1))
     # Calculate the margin of error for a 95% confidence interval
-  margin_of_error_X1 <- qt(0.975, df = length(tissue_cumulative2$X1) - 1) * se_mean_X1
+  margin_of_error_X1 = qt(0.975, df = length(tissue_cumulative2$X1) - 1) * se_mean_X1
   
   # Calculate the standard error of the mean
-  se_mean_X2 <- sd(tissue_cumulative2$X2) / sqrt(length(tissue_cumulative2$X2))
+  se_mean_X2 = sd(tissue_cumulative2$X2) / sqrt(length(tissue_cumulative2$X2))
   # Calculate the margin of error for a 95% confidence interval
-  margin_of_error_X2 <- qt(0.975, df = length(tissue_cumulative2$X2) - 1) * se_mean_X2
+  margin_of_error_X2 = qt(0.975, df = length(tissue_cumulative2$X2) - 1) * se_mean_X2
   
   
   write.csv(tissue_cumulative, file=paste("./shuffling/ROC_shuffle_",tissue,".csv",sep=""))
