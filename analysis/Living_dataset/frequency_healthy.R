e0fbb3fca3cf9b14de132cb44f08a64c89768ffb
Anaskou <75255312+Anaskou@users.noreply.github.com>
2024-04-23 15:32:43 +0300

    Update frequency_healthy.R

--- analysis/Living_dataset/frequency_healthy.R
+++ analysis/Living_dataset/frequency_healthy.R
@@ -53,7 +53,7 @@ library(wesanderson)
 ######################################### HEALTHY DATASET ###################################################################
 
 #Upload healhy dataset
-data_healthy_first=read.delim("/mnt/raid1/argis/GTEx/after_access/normal_samples_OtherDatesets/tissues/PRJEB4337/Agamemnon_after_bowtie/final/all/Agamemnon_after_bowtie_species_PRJEB44337_11Tissues.tab")
+data_healthy_first=read.delim("/mnt/raid1/argis/GTEx/after_access/normal_samples_OtherDatesets/tissues/PRJEB4337/Agamemnon_after_bowtie/final/all/Agamemnon_after_bowtie_species_PRJEB44337.tab")
 metadata_healthy=readRDS("/mnt/raid1/argis/GTEx/after_access/normal_samples_OtherDatesets/metadata_healthy.Rdata")
 rownames(data_healthy_first)=data_healthy_first$External.ID
 data_healthy_first=data_healthy_first[,2:length(colnames(data_healthy_first))]
@@ -83,7 +83,7 @@ table(metadata_healthy$tissue)
 data_normalised_healthy_first[1:5,1:5]
 
 # Convert values to relative frequency
-data_relative_healthy <- as.data.frame(lapply(data_normalised_healthy_first, function(x) x / sum(x)))
+data_relative_healthy = as.data.frame(lapply(data_normalised_healthy_first, function(x) x / sum(x)))
 data_relative_tissues=data.frame(matrix(nrow=length(data_normalised_healthy_first[,1]), ncol =1))
 for  (tissue in unique(metadata_healthy$tissue)) {
   data_relative_health_tissue=data_relative_healthy[,which(metadata_healthy$tissue%in%tissue)]
@@ -99,15 +99,15 @@ write.csv(data_relative_tissues,file="Relative_Frequency_per_tissue_living_datas
 # For liver
 data_relative_tissues$microbiomes= rownames(data_relative_tissues)
 liver_relative=as.data.frame(data_relative_tissues[,c("Liver","microbiomes")],)
-liver_relative_sorted <- as.data.frame(liver_relative[order(liver_relative[,1],decreasing = TRUE), ])
-features <- rownames(liver_relative_sorted)[1:5]
-values <- liver_relative_sorted[1:5,1]  # Example values for the features
+liver_relative_sorted = as.data.frame(liver_relative[order(liver_relative[,1],decreasing = TRUE), ])
+features = rownames(liver_relative_sorted)[1:5]
+values = liver_relative_sorted[1:5,1]  # Example values for the features
 
 # Create a dataframe with features and their corresponding values
-data_liver <- data.frame(features, values)
-data_liver$features <- str_wrap(data_liver$features, width = 2)  # Adjust width as needed
+data_liver = data.frame(features, values)
+data_liver$features = str_wrap(data_liver$features, width = 2)  # Adjust width as needed
 
-pie_chart <- ggplot(data_liver, aes(x = "", y = values, fill = features)) +
+pie_chart = ggplot(data_liver, aes(x = "", y = values, fill = features)) +
   geom_bar(stat = "identity", width = 1) +
   geom_text(aes(x = 1.3, label = paste(features, "\n", round(values*100),"%"), angle=0, size =14), 
             position = position_stack(vjust = 0.5)) +
@@ -131,15 +131,15 @@ dev.off()
 # For Colon
 data_relative_tissues$microbiomes= rownames(data_relative_tissues)
 colon_relative=as.data.frame(data_relative_tissues[,c("Colon","microbiomes")],)
-colon_relative_sorted <- as.data.frame(colon_relative[order(colon_relative[,1],decreasing = TRUE), ])
-features <- rownames(colon_relative_sorted)[1:5]
-values <- colon_relative_sorted[1:5,1]  # Example values for the features
+colon_relative_sorted = as.data.frame(colon_relative[order(colon_relative[,1],decreasing = TRUE), ])
+features = rownames(colon_relative_sorted)[1:5]
+values = colon_relative_sorted[1:5,1]  # Example values for the features
 
 # Create a dataframe with features and their corresponding values
-data_colon <- data.frame(features, values)
-data_colon$features <- str_wrap(data_colon$features, width = 2)  # Adjust width as needed
+data_colon = data.frame(features, values)
+data_colon$features = str_wrap(data_colon$features, width = 2)  # Adjust width as needed
 
-pie_chart <- ggplot(data_colon, aes(x = "", y = values, fill = features)) +
+pie_chart = ggplot(data_colon, aes(x = "", y = values, fill = features)) +
   geom_bar(stat = "identity", width = 1) +
   geom_text(aes(x = 1.3, label = paste(features, "\n", round(values*100),"%"), angle=0, size =14), 
             position = position_stack(vjust = 0.5)) +
@@ -161,15 +161,15 @@ dev.off()
 # For Heart
 data_relative_tissues$microbiomes= rownames(data_relative_tissues)
 heart_relative=as.data.frame(data_relative_tissues[,c("Heart","microbiomes")],)
-heart_relative_sorted <- as.data.frame(heart_relative[order(heart_relative[,1],decreasing = TRUE), ])
-features <- rownames(heart_relative_sorted)[1:5]
-values <- heart_relative_sorted[1:5,1]  # Example values for the features
+heart_relative_sorted = as.data.frame(heart_relative[order(heart_relative[,1],decreasing = TRUE), ])
+features = rownames(heart_relative_sorted)[1:5]
+values = heart_relative_sorted[1:5,1]  # Example values for the features
 
 # Create a dataframe with features and their corresponding values
-data_heart <- data.frame(features, values)
-data_heart$features <- c(str_wrap(data_heart$features[1], width = 2), data_heart$features[2:5])  # Adjust width as needed
+data_heart = data.frame(features, values)
+data_heart$features = c(str_wrap(data_heart$features[1], width = 2), data_heart$features[2:5])  # Adjust width as needed
 
-pie_chart <- ggplot(data_heart, aes(x = "", y = values, fill = features)) +
+pie_chart = ggplot(data_heart, aes(x = "", y = values, fill = features)) +
   geom_bar(stat = "identity", width = 1) +
   geom_text(aes(x = 1.3, label = paste(features,"(", round(values*100),"%)"), angle=50, size =14), 
             position = position_stack(vjust = 0.5)) +
